60dbe910ec3703bc84e11af6d937ff9798621f71
halldorajona <halldorajona@gmail.com>
2015-11-18 22:36:08 +0000

    register

--- routes/register.js
+++ routes/register.js
@@ -1,16 +1,24 @@
 'use strict';
 
 var express = require('express');
-var router = express.Router();
+var validate = require('../lib/validate');
 var dbUtils = require('../utils/db-utils');
-var loggedin = false;
 var bcrypt = require('bcrypt-nodejs');
 var xss = require('xss');
+var router = express.Router();
+var loggedin = false;
 var registered = false;
+var nameerror = true;
+var emailerror = true;
+var usernameput, passwordput, nameput, emailput;
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('register', {loggedin:loggedin});
+  res.render('register', {loggedin:loggedin,
+                          usernameput : '',
+                          passwordput : '',
+                          nameput : '',
+                          emailput : ''});
 });
 
 /* GET home page. */
@@ -20,6 +28,11 @@ router.post('/', function(req, res) {
   var parameters = [req.body.username, hash];
   var clean = xss(req.body.username);
   clean = xss(req.body.password);
+
+  usernameput = req.body.username;
+  passwordput = req.body.password;
+  nameput = req.body.heiti;
+  emailput = req.body.email;
 
   dbUtils.queryDb(queryStr, parameters, function(err) {
     if(err) {
@@ -31,26 +44,37 @@ router.post('/', function(req, res) {
       return console.error('error fetching client from pool', err);
     }
 
-    if(req.body.username === ""){
+    if(usernameput === ""){
       var nousername = true;
       registered = false;
-      res.render('register', {registered:registered,
-                              nousername:nousername,
-                              loggedin:loggedin});
-      return console.error('error fetching client from pool', err);
     }
 
-    if(req.body.password === ""){
+    if(passwordput === ""){
       var nopassword = true;
       registered = false;
-      res.render('register', {registered:registered,
-                              nopassword:nopassword,
-                              loggedin:loggedin});
-      return console.error('error fetching client from pool', err);
     }
 
-    registered = true;
-    res.render('register',{registered:registered, loggedin:loggedin});
+    if(!validate.length1(req.body.heiti, 3)){
+      nameerror = false;
+      registered = false;
+    }
+    if(!validate.isEmail(req.body.email)){
+      emailerror = false;
+      registered = false;
+    }
+
+    registered = true
+
+    res.render('register', {registered:registered,
+                            nousername:nousername,
+                            nopassword:nopassword,
+                            nameerror:nameerror,
+                            emailerror:emailerror,
+                            loggedin:loggedin,
+                            usernameput:usernameput,
+                            emailput:emailput,
+                            nameput:nameput});
+    return console.error('error fetching client from pool', err);
   });
 });
 
