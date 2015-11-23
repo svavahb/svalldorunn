62e044b6df7ca487e2d0b84bcb646edf57b4d433
unknown <svavahb@gmail.com>
2015-11-23 13:04:18 +0000

    bla

--- routes/entries.js
+++ routes/entries.js
@@ -5,9 +5,12 @@ var router = express.Router();
 var dbUtils = require('../utils/db-utils');
 var loggedin = true;
 var xss = require('xss');
+var threadId;
 
-router.get('/:id', ensureLoggedIn, prufurender);
-router.post('/',postEntries, prufurender);
+router.get('/:id', ensureLoggedIn, getRender);
+router.post('/:id', postEntries, postRender); // HALLO HJALP
+// http://localhost:8080/entries/:id
+
 
 function ensureLoggedIn(req, res, next){
     if(req.session.user){
@@ -18,43 +21,54 @@ function ensureLoggedIn(req, res, next){
     }
 }
 
-function prufurender(req, res) {
-    var login = "select * FROM entries WHERE threadid=$1 ORDER BY date DESC LIMIT 20";
-    console.log(req.session);
-    console.log(req.params.id);
-    var parameters = [req.params.id];
-    var createThread;
+function getRender(req, res) {
+  var login = "select * FROM entries WHERE threadid=$1 ORDER BY date DESC LIMIT 20";
+  console.log(req.session);
+  threadId = req.params.id;
+  var parameters = [req.params.id];
 
-    dbUtils.queryDb(login, parameters, function(err,result) {
-      if(err) {
-        res.render('login', {loggedin:loggedin});
-        return console.error('error fetching client from pool', err);
-      }
-      var entries = result;
-      var imageset;
-      var usern = req.session.user.username;
-
-      if(req.session.user.image===null) {
-        imageset=false;
-      }
-      else {
-        imageset=true;
-      }
+  dbUtils.queryDb(login, parameters, function(err,result) {
+    if(err) {
+      res.render('login', {loggedin:loggedin});
+      return console.error('error fetching client from pool', err);
+    }
+    var entries = result;
+    var usern = req.session.user.username;
 
     res.render('entries', {session : req.session,
                           loggedin:loggedin,
                           entries:entries,
                           usern:usern,
-                          imageset:imageset});
-    });
+                        threadId:threadId});
+  });
+}
+
+function postRender(req, res) {
+  var login = "select * FROM entries WHERE threadid=$1 ORDER BY date DESC LIMIT 20";
+  console.log(req.session);
+  var parameters = [req.params.id];
+
+  dbUtils.queryDb(login, parameters, function(err, result) {
+    if(err) {
+      res.render('login', {loggedin:loggedin});
+      return console.error('error fetching client from pool', err);
+    }
+    var entries = result;
+    var usern = req.session.user.username;
+
+    res.render('entries', {session : req.session,
+                          loggedin:loggedin,
+                          entries:entries,
+                          usern:usern});
+  });
 }
 
 function postEntries(req, res, next) {
-  var entrie = "INSERT INTO entries (username, entry, date, threadid) VALUES ($1, $2, $3, $4)";
-  var info = [req.session.user.username, req.body.textarea, new Date(), req.params.id];
+  var entry = "INSERT INTO entries (username, entry, date, threadid) VALUES ($1, $2, $3, $4)";
+  var info = [req.session.user.username, req.body.textarea, new Date(), threadId];
   var clean = xss(req.body.textarea);
 
-  dbUtils.queryDb(entrie, info, function(err) {
+  dbUtils.queryDb(entry, info, function(err) {
     if(err){
       return console.error('error fetching entries from pool', err);
     }
